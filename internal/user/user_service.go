b3ffd629c5336541bd3e97ef28edad23e118bcb5
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-18 10:31:57 -0300

    feat(user): implement get-user-by-email service

--- internal/user/user_service.go
+++ internal/user/user_service.go
@@ -2,13 +2,27 @@ package user
 
 import (
 	"context"
+	"errors"
+	"os"
 	"server/util"
+	"strconv"
 	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
+
+var secretKey = os.Getenv("JWT_SECRET_KEY")
 
 type service struct {
 	Repository
 	timeout time.Duration
+}
+
+func NewService(repository Repository) Service {
+	return &service{
+		repository,
+		time.Duration(2) * time.Second,
+	}
 }
 
 func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error) {
@@ -21,7 +35,13 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 		return nil, err
 	}
 
-	u := &User{
+	u, _ := s.Repository.GetUserByEmail(ctx, req.Email)
+
+	if u != nil {
+		return &CreateUserRes{}, errors.New("email already in use")
+	}
+
+	u = &User{
 		Username: req.Username, Email: req.Email, Password: hashedPassword,
 	}
 
@@ -40,9 +60,39 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	return res, nil
 }
 
-func NewService(repository Repository) Service {
-	return &service{
-		repository,
-		time.Duration(2) * time.Second,
+type MyJWTClaims struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func (s *service) Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, err
 	}
+
+	err = util.CheckPassword(req.Password, u.Password)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyJWTClaims{
+		ID:       strconv.Itoa(int(u.Id)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    strconv.Itoa(int(u.Id)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{accessToken: ss, Username: u.Username, Id: strconv.Itoa(int(u.Id))}, nil
 }
