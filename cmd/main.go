44bedccaa6fe3a8cd4c4551643ad51f38cd5fe02
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-18 09:58:36 -0300

    feat: add godotenv package

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,17 @@ import (
 	"server/db"
 	"server/internal/user"
 	"server/router"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	dbConn, err := db.NewDatabase()
 
 	if err != nil {
